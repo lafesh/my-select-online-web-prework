6af27cbf94fcdd87d30cbe1cb557996b312baed2
Gina Fedder <ginafedder@yahoo.com>
2018-09-20 18:53:20 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+ i = 0 
+ new_collection = []
+ while i < collection.length 
+    yield(collection[i])
+    i = i + 1 
+    if i == true
+      new_collection.push(i) 
+    end
+      
+ end 
+ new_collection
 end
