61f340738f1367677f96f8c1e11ea067ce6a2da9
Gina Fedder <ginafedder@yahoo.com>
2018-09-20 19:26:02 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,21 +2,10 @@ def my_select(collection)
  i = 0 
  new_collection = []
  while i < collection.length 
-
     if yield(collection[i])
-    
       new_collection.push(collection[i]) 
     end
     i = i + 1  
  end 
  new_collection
-end
-
-
-my_select([0,1,2,3]) do |element|
-  
-end
-
-array.select do |element|
-  element.even?
 end
